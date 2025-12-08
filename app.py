905d953e42246d715f52fafba7969872d304662b
Kim Martini <dr.kim.martini@gmail.com>
2025-12-08 10:35:36 -0800

    Updated app
    Now includes calculations for pH

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.markdown(
 )
 
 # Set up the Streamlit app
-st.title("pCO2 to GO Calculator")
+st.title("pCO\u2082 to Go Calculator")
 st.markdown(
     """
     This application calculates the required reagent to achieve a target 
@@ -27,15 +27,17 @@ st.markdown(
 # make a title
 st.sidebar.markdown("# :blue[Starting Properties]")
 
-# *** Starting Water *****
+# *** SOURCE/STARTING WATER *****
 # Select calculation type
+st.sidebar.markdown("### Select the measurement pair for the starting water")
+# define the calculation type
 input_type = st.sidebar.selectbox(
     "Select your measurement pair:",
-    ["pCO2 and Alkalinity", "pCO2 and pH [coming soon!]", "pCO2 and Total CO2/DIC"]
+    ["pCO2 and Alkalinity", "pCO2 and pH", "pCO2 and Total CO2/DIC"], 
+    label_visibility="collapsed"
 )
-
 # Input current water properties
-st.sidebar.markdown("### Input current water properties")
+st.sidebar.markdown("### Input starting water properties")
 # define the input pCO2
 source_pCO2 = st.sidebar.number_input(
         label="pCO\u2082 [µatm]",
@@ -45,7 +47,7 @@ source_pCO2 = st.sidebar.number_input(
         value = 400,
         key="source_pCO2",
 )
-# define alkalinity or tCO2 depending on the calculation type
+# define alkalinity, pH, or tCO2 depending on the calculation type
 if input_type == "pCO2 and Alkalinity":
     source_alkalinity = st.sidebar.number_input(
         label = "Alkalinity [µmol/kg]",
@@ -55,27 +57,37 @@ if input_type == "pCO2 and Alkalinity":
         value=2300,
         key="source_alkalinity"
     )
-    
 elif input_type == "pCO2 and Total CO2/DIC":
     source_tCO2 = st.sidebar.number_input(
-        label = "Total CO\u2082 [µmol/kg]",
+        label = "Total CO\u2082/DIC [µmol/kg]",
         min_value = 20,
         max_value = 5000,
         step = 10,
         value=2000,
         key="source_tCO2"
     )
+elif input_type == "pCO2 and pH":
+    source_pH = st.sidebar.number_input(
+        label = "pH ",
+        min_value = 6.0,
+        max_value = 9.0,
+        step = 0.1,
+        value= 7.0,
+        key="source_pH"
+    )
 # define the water temperature and salinity
-temperature = st.sidebar.number_input(
+source_temperature = st.sidebar.number_input(
     label = "Water temperature [°C]",
-    min_value = -2,
-    max_value = 38,
-    step = 1,
-    value=25, 
+    format="%.1f",
+    min_value = -2.0,
+    max_value = 38.0,
+    step = 1.0,
+    value=25.0, 
     key="source_temperature"
 )
 salinity = st.sidebar.number_input(
     label="Salinity [PSU]",
+    format="%.1f",
     min_value = 0,
     max_value = 40,
     step = 1,
@@ -86,29 +98,41 @@ salinity = st.sidebar.number_input(
 ## Perform calculations based on selected type
 if input_type == "pCO2 and Alkalinity":
     source = cc.calculate_carbonate_chemistry_pCO2_alkalinity(
-        temperature,
+        source_temperature,
         salinity,
         source_pCO2,
         source_alkalinity
     )
-
 elif input_type ==  "pCO2 and Total CO2/DIC":
     source = cc.calculate_carbonate_chemistry_pCO2_tCO2(
-        temperature,
+        source_temperature,
         salinity,
         source_pCO2,
         source_tCO2
     )
+elif input_type == "pCO2 and pH":
+    source = cc.calculate_carbonate_chemistry_pCO2_pH(
+        source_temperature,
+        salinity,
+        source_pCO2,
+        source_pH
+    )
 
-
+# *** TARGET WATER *****
 # Input the target parameters
 # This is Omega Aragonite or pH
-st.sidebar.markdown("# :green[Target State]")
+st.sidebar.markdown("# :orange[Target State]")
+# Select calculation type
+st.sidebar.markdown("### Select the measurement pair for the target water")
 # Select calculation type
 target_type = st.sidebar.selectbox(
     "Select your target pair:",
-    ["pCO2 and Ω aragonite", "pCO2 and pH [coming soon!]"]
+    ["pCO2 and Ω aragonite", "pCO2 and pH"], 
+    label_visibility="collapsed"
 )
+
+# Select calculation type
+st.sidebar.markdown("### Input target water properties")
 # define the target pCO2
 target_pCO2 = st.sidebar.number_input(
         label="pCO\u2082 [µatm]",
@@ -122,22 +146,13 @@ target_pCO2 = st.sidebar.number_input(
 if target_type == "pCO2 and Ω aragonite":
     target_OmegaAragonite = st.sidebar.number_input(
         label = "Ω aragonite ",
-        min_value = 0.0,
-        max_value = 4.0,
-        step = 0.1,
-        value= 4.0,
-        format ="%.1f",
+        min_value = 0,
+        max_value = 4,
+        step = 1, 
+        value= 4,
         key="target_OmegaAragonite"
     )
-    # do the calculation for target omega aragonite
-    target = cc.calculate_carbonate_chemistry_pCO2_OmegaA(
-    temperature,
-    salinity,
-    target_pCO2,
-    target_OmegaAragonite
-    )
-    print(target)
-elif input_type == "pCO2 and pH":
+elif target_type == "pCO2 and pH":
     target_pH = st.sidebar.number_input(
         label = "pH ",
         min_value = 6.0,
@@ -146,30 +161,91 @@ elif input_type == "pCO2 and pH":
         value= 8.1,
         key="target_pH"
     )
+# define the water temperature and salinity
+target_temperature = st.sidebar.number_input(
+    label = "Water temperature[°C]",
+    format="%.1f",
+    min_value = -2.0,
+    max_value = 38.0,
+    step = 1.0,
+    value=source['temperature'], 
+    key="target_temperature"
+)
+if target_type == "pCO2 and Ω aragonite":
+    # do the calculation for target omega aragonite
+    target = cc.calculate_carbonate_chemistry_pCO2_OmegaA(
+    target_temperature,
+    salinity,
+    target_pCO2,
+    target_OmegaAragonite
+    )
+    print(target)
+elif target_type == "pCO2 and pH":
     # do the calculation for target pH
+    target = cc.calculate_carbonate_chemistry_pCO2_pH(
+        target_temperature,
+        salinity,
+        target_pCO2,
+        target_pH
+)
+    
+    
 
 # ****** SIDEBAR FOR THE REAGENT PARAMETERS *****
-st.sidebar.markdown("# :orange[Reagent Properties]")
+# select the volume units
+st.sidebar.markdown("### Reagent Properties and Target Volumes")
+
+# select volume units
+volume_units = st.sidebar.selectbox(
+    "Select volume units:",
+    ["Liters", "Gallons", "Cubic Meters"]
+)
+
+if volume_units == "Liters":
+    vol_units_label = "L"
+elif volume_units == "Gallons":
+    vol_units_label = "gal"
+elif volume_units == "Cubic Meters":
+    vol_units_label = "m³"
+
+buffer_volume_label = "Target System/Tank Volume  [" + vol_units_label + "]"
+
+buffer_volume = st.sidebar.number_input(
+    buffer_volume_label,
+    value=16.0, 
+    key="Buffered_volume"
+)
+
+if volume_units == "Liters":
+    st.sidebar.info("Note: All volume outputs will be in Liters.")
+elif volume_units == "Gallons":
+    st.sidebar.info("Note: All volume outputs will be in Gallons.")
+elif volume_units == "Cubic Meters":
+    st.sidebar.info("Note: All volume outputs will be in Cubic Meters.")
+
+# select the reagent properties
 carbonate_reagent_alkalinity = st.sidebar.number_input(
     "Carbonate Reagent Alkalinity  [µmol/kg]",
-    value=330045,
+    value=330000,
+    format="%.2e",
+    step = 1000,
     key="CRgt_Alk"
 )
 carbonate_reagent_TCO2 = st.sidebar.number_input(
     "Carbonate Reagent tCO\u2082/DIC  [µmol/kg]",
-    value=20037,
+    value=20000,
+    format="%.2e",
+    step = 1000,
     key="CRgt_TCO2"
 )
 acid_reagent_HCL = st.sidebar.number_input(
     "Acid Reagent Concentration  [µmol/kg]",
     value=100000,
+    format="%.2e",
+    step = 1000,
     key="Argt_HCL"
 )
-buffer_volume = st.sidebar.number_input(
-    "Buffer Volume  [L]",
-    value=16.0, 
-    key="Buffered_volume"
-)
+
 
 # calculate the required reagent volumes
 reagents = cc.manipulator(
@@ -185,7 +261,7 @@ reagents = cc.manipulator(
 
 # ***** DISPLAY RESULTS *****
 
-
+# Display the starting water summary
 st.subheader(":blue[Starting Water Property Summary]")
 st.markdown("Calculated properties of the starting water:")
 
@@ -197,9 +273,9 @@ starting_summary =  {
     "Ω aragonite": [f"{source['Omega_aragonite']:.0f}"]
 }
 st.dataframe(starting_summary, hide_index=True)
-# st.table(starting_summary)
 
-st.subheader(":green[Target State Summary]")
+# Display the target water summary
+st.subheader(":orange[Target State Summary]")
 st.markdown("Calculated properties of the final water after reagent addition:")
 target_summary =  {
     "pCO2": [f"{target['pCO2']:.0f} µatm"],
@@ -210,10 +286,10 @@ target_summary =  {
 }
 st.dataframe(target_summary, hide_index=True)
 
-st.subheader(":orange[Target Reagent Volumes]")
+# Display reagent volumes
+st.markdown("Calculated Reagent Volumes to achieve target state:")
 reagent_summary = {
-    "Target Ω aragonite": [f"{target_OmegaAragonite:.1f}"],
-    "Carbonate Reagent Volume": [f"{reagents['CrgtVol']:.2f} L"],
-    "Acid Reagent Volume": [f"{reagents['ArgtVol']:.2f} L"]
+    "Carbonate Reagent Volume": [f"{reagents['CrgtVol']:.2f} "+vol_units_label],
+    "Acid Reagent Volume": [f"{reagents['ArgtVol']:.2f} "+vol_units_label]
 }
 st.dataframe(reagent_summary, hide_index=True)
